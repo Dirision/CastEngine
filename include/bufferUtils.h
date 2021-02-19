e14730c00381c0d68f9807d8b5e255203d68aa3d
dirision <nick.dirisio@mail.utoronto.ca>
2021-02-19 11:35:01 -0500

    Moving to cpp early

--- include/bufferUtils.h
+++ include/bufferUtils.h
@@ -1,1 +1,9 @@
+#ifndef BUFFERUTILS_H
+#define BUFFERUTILS_H
+
 void printTest(void);
+
+int writeBufferToFile(int *imgBuffer,int width, int height);
+
+
+#endif
---
+++ include/engine.h
@@ -0,0 +1,11 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+// class for our pixel buffer object, a 3*height*width array containing all the pixel color data
+class buffer { 
+
+};
+
+
+// Creates int array of width * height representing the display buffer
+int* createBuffer(int width, int height);
+#endif
